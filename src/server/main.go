10bf366a53ef1532a795ec00e66a21c3b6800fa1
Thomas Kao <thomaskao@wirelessprv-10-194-181-195.near.illinois.edu>
2017-10-06 21:31:10 -0500

    10/6 testing

--- src/server/main.go
+++ src/server/main.go
@@ -84,25 +84,47 @@ func Listen(port int) {
 			if(memberList.Size() < 2 && id != entryMachineId) {
 				continue
 			}
+
+			timeout := make(chan bool, 1)
+			go func() {
+				time.Sleep(detectionTime)
+				timeout <- true
+			}()
+
+
 			select {
 			case <- leave:
 				fmt.Println("Stop Listen")
 				break ListenLoop
-			case <- time.After(detectionTime):
-				currNode := memberList.GetNode(id)
-				failedId := getNeighbor(port-8000, currNode)
-				if failedId == 0 {
-					continue
-				}
-				failedNode := memberList.GetNode(failedId)
-				failedNode.SetStatus(FAILED)
-				failedNode.IncrementHBCounter()
-				log.Printf("Machine %d failed", failedId)
-				go Cleanup(failedId)
-
+			//case <- time.After(detectionTime):
+			//	currNode := memberList.GetNode(id)
+			//	failedId := getNeighbor(port-8000, currNode)
+			//	if failedId == 0 {
+			//		continue
+			//	}
+			//	failedNode := memberList.GetNode(failedId)
+			//	failedNode.SetStatus(FAILED)
+			//	failedNode.IncrementHBCounter()
+			//	log.Printf("Machine %d failed", failedId)
+			//	go Cleanup(failedId)
 			default:
 				buffer := make([]byte, 1024)
 				conn, err := net.ListenUDP("udp", udpAddr)
+				conn.SetReadDeadline(time.Now().Add(detectionTime))
+				if err, ok := err.(net.Error); ok && err.Timeout() {
+					// Didn't recieve heartbeat
+					currNode := memberList.GetNode(id)
+					failedId := getNeighbor(port-8000, currNode)
+					if failedId == 0 {
+						continue
+					}
+					failedNode := memberList.GetNode(failedId)
+					failedNode.SetStatus(FAILED)
+					failedNode.IncrementHBCounter()
+					log.Printf("Machine %d failed", failedId)
+					go Cleanup(failedId)
+					continue
+				}
 				conn.ReadFrom(buffer)
 				conn.Close()
 
