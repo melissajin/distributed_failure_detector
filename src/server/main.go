6af68d215753421716f2bb72530616adcb71cff1
Thomas Kao <thomaskao@Thomass-MacBook-Air.local>
2017-10-08 16:16:36 -0500

    10/8: change bandwidth

--- src/server/main.go
+++ src/server/main.go
@@ -27,7 +27,7 @@ const (
 	cleanupTime = time.Second * 6
 	detectionTime = time.Second * 2
 	startupTime = time.Second * 1
-	heartbeatInterval = time.Millisecond * 600
+	heartbeatInterval = time.Millisecond * 700
 	messageLossRate = 30
 )
 
