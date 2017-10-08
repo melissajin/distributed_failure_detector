4e6a1d0e467a56d6773be3570aae6237884eedef
Melissa  Jin <melissajin@Melissas-MacBook-Pro.local>
2017-10-08 17:54:22 -0500

    modified sim drop messages

--- src/server/main.go
+++ src/server/main.go
@@ -28,7 +28,7 @@ const (
 	cleanupTime = time.Second * 6
 	detectionTime = time.Second * 2
 	startupTime = time.Second * 1
-	heartbeatInterval = time.Millisecond * 500
+	heartbeatInterval = time.Millisecond * 400
 	messageLossRate = 30
 )
 
